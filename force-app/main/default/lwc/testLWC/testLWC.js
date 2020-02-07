1f4ceb1ae80b3e8f727bb7a9e104824182c24153
lolitav1998 <lolitav1998@gmail.com>
2020-02-07 16:26:14 +0200

    LWC fixes

--- force-app/main/default/lwc/testLWC/testLWC.js
+++ force-app/main/default/lwc/testLWC/testLWC.js
@@ -5,22 +5,18 @@ const columnList = [
     {label: 'Name', fieldName: 'Name'},
     {label: 'Phone', fieldName: 'Phone'}
 ];
-    export default class DataRetriever extends LightningElement {
-        @track dataSet;
-        @track error;
-        @track columnList = columnList;
-        keyChanged(event) {
-            window.clearTimeout(this.delayTimeout);
-            const searchKey = event.target.value;
-            // eslint-disable-next-line @lwc/lwc/no-async-operation
-            this.delayTimeout = setTimeout(() => {
-            getMatchesList({ searchKey }).then(result => {
-                this.dataSet = result;
-                this.error = undefined;
-            }).catch(error => {
-                this.error = error;
-                this.dataSet = undefined;
-            });
-        }, 350);
+export default class DataRetriever extends LightningElement {
+    @track dataSet;
+    @track error;
+    @track columnList = columnList;
+    keyChanged(event) {
+        const searchKey = event.target.value;
+        getMatchesList({ searchKey }).then(result => {
+            this.dataSet = result;
+            this.error = undefined;
+        }).catch(error => {
+            this.error = error;
+            this.dataSet = undefined;
+        });
     }
 }
